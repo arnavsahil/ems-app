f02632dae53ff97da49c7fdd477351cc9c19fdad
Arnav <arnavsahil3@gmail.com>
2026-01-14 19:03:43 +0530

    admin dashboard ui done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
 
 const App = () => {
   return (
@@ -11,6 +12,10 @@ const App = () => {
         <Route
           path="/dash-employee"
           element={<EmployeeDashboard></EmployeeDashboard>}
+        ></Route>
+        <Route
+          path="/dash-admin"
+          element={<AdminDashboard></AdminDashboard>}
         ></Route>
       </Routes>
     </>
