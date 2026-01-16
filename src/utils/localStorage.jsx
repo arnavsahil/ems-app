44d6809a355e083f4f24b7aaf8ffff6e458deab4
Arnav <arnavsahil3@gmail.com>
2026-01-16 16:46:21 +0530

    added authprovider file and changes in handleLogin to access employees using localStorage data

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -217,8 +217,8 @@ export const setLocalStorage = () => {
 export const getLocalStorage = () => {
   const employees = JSON.parse(localStorage.getItem("employees"));
   const admin = JSON.parse(localStorage.getItem("admin"));
-  console.log(JSON.parse(employees));
-  console.log(JSON.parse(admin));
+  // console.log(JSON.parse(employees));
+  // console.log(JSON.parse(admin));
 
-  // return { employees, admin };
+  return { employees, admin };
 };
